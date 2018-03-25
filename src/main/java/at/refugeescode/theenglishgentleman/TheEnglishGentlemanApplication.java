336476f0ae6327dbb253d5564f3aedbbd55adc10
Alireza <alireza@Alirezas-MacBook-Pro.local>
2018-03-25 16:55:19 +0200

    Add project structure

---
+++ src/main/java/at/refugeescode/theenglishgentleman/TheEnglishGentlemanApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.theenglishgentleman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheEnglishGentlemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheEnglishGentlemanApplication.class, args);
+	}
+}
